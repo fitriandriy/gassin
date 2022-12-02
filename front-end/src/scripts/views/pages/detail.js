fe37f13960ab472b7ed09052d0ef833966b9c909
firay24 <72513085+Firay24@users.noreply.github.com>
2022-12-02 12:53:42 +0700

    Front-end Result

--- front-end/src/scripts/views/pages/detail.js
+++ front-end/src/scripts/views/pages/detail.js
@@ -6,6 +6,61 @@ import LikeButtonInitiator from '../../utils/like-button-initiator';
 const Detail = {
   async render() {
     return `
+      <div class="head-room">
+        <div><h1>Choose Yours Schedule</h1></div>
+        <div class="room-info">
+          <div><img src="../../public/room.png" alt=""></div>
+          <div>
+              <p>Room Id</p>
+              <p>Event</p>
+              <p>Username</p>
+          </div>
+        </div>
+      </div>
+      <div class="schedules">
+          <div class="schedule">
+              <h5>25 September 2022</h5>
+              <div class="hours">
+                  <div class="hour-title">
+                      <p>start</p>
+                      <p>end</p>
+                  </div>
+                  <div class="hour">
+                      <input type="checkbox">
+                      <input type="text">
+                      <input type="text">
+                  </div>
+                  <div class="hour">
+                      <input type="checkbox">
+                      <input type="text">
+                      <input type="text">
+                  </div>
+              </div>
+          </div>
+          <div class="schedule">
+              <h5>26 September 2022</h5>
+              <div class="hours">
+                  <div class="hour-title">
+                      <p>start</p>
+                      <p>end</p>
+                  </div>
+                  <div class="hour">
+                      <input type="checkbox">
+                      <input type="text">
+                      <input type="text">
+                  </div>
+                  <div class="hour">
+                      <input type="checkbox">
+                      <input type="text">
+                      <input type="text">
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="save-button">
+          <button class='button-logout'>logout</button>
+          <button class='button-voting'>create voting</button>
+      </div>
       <div id="movie" class="movie"></div>
       <div id="likeButtonContainer"></div>
     `;
